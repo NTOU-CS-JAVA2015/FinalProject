7ebf052b9a029e578006cd67ff75f4260ed33748
vic4113110631 <vic4113110631@yahoo.com.tw>
2016-01-07 05:56:44 +0800

    Update AboutMenu.java
    Update:Yes or No for href。

--- MDIEditor/src/AboutMenu.java
+++ MDIEditor/src/AboutMenu.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionListener;
 import java.io.IOException;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 
 public class AboutMenu {
 
@@ -54,7 +55,14 @@ public class AboutMenu {
                     url = "https://github.com/vic4113110631";
                     break;
             }
-            Runtime.getRuntime().exec("cmd /c start " + url);
+            int dialogButton = JOptionPane.YES_NO_OPTION;
+                String href = "LINK to ".concat(e.getActionCommand().concat(" github"));
+            if (JOptionPane.showConfirmDialog(null, href, "True or false?",
+                JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
+                Runtime.getRuntime().exec("cmd /c start " + url);
+            } else {
+                // no option
+             }
         } catch (IOException ioe) {
             System.err.println(ioe.toString());
         }
